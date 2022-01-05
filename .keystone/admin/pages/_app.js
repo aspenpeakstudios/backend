1056c80cab2a270e12c7cf32a051b2a17aac3cdb
bissenj <bissenj@gmail.com>
2022-01-04 22:03:06 -0700

    Changes up to L19

--- .keystone/admin/pages/_app.js
+++ .keystone/admin/pages/_app.js
@@ -8,8 +8,10 @@ import * as view898e6201 from '../../../node_modules/@keystone-next/fields/types
 import * as viewabfaeff6 from '../../../node_modules/@keystone-next/fields/types/text/views';
 import * as view73292056 from '../../../node_modules/@keystone-next/fields/types/password/views';
 import * as viewaad7c01c from '../../../node_modules/@keystone-next/fields/types/timestamp/views';
+import * as viewb361bdb4 from '../../../node_modules/@keystone-next/fields/types/relationship/views';
 import * as view335f2625 from '../../../node_modules/@keystone-next/fields/types/select/views';
 import * as view66038237 from '../../../node_modules/@keystone-next/fields/types/integer/views';
+import * as viewaabb95fd from '../../../node_modules/@keystone-next/cloudinary/views';
 
 const adminConfig = {};
 
@@ -18,8 +20,10 @@ const fieldViews = {
   viewabfaeff6,
   view73292056,
   viewaad7c01c,
+  viewb361bdb4,
   view335f2625,
   view66038237,
+  viewaabb95fd,
 };
 
 const lazyMetadataQuery = {
@@ -194,7 +198,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="1vsf718"
+        adminMetaHash="dsu3c7"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
--- keystone.ts
+++ keystone.ts
@@ -7,6 +7,8 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -40,11 +42,18 @@ export default withAuth(
       adapter: 'mongoose',
       url: databaseURL,
       // TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the database.');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // Schema items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // TODO: change this for roles.
