6fce85977124bb6260baadb5550f11d010208468
bissenj <bissenj@gmail.com>
2022-01-14 17:29:52 -0700

    Up to L51

--- keystone.ts
+++ keystone.ts
@@ -11,6 +11,7 @@ import { ProductImage } from './schemas/ProductImage';
 import { CartItem } from './schemas/CartItem';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -65,6 +66,7 @@ export default withAuth(
       ProductImage,
       CartItem,
     }),
+    extendGraphqlSchema,
     ui: {
       // TODO: change this for roles.
       // Show UI only for people who pass this test.
